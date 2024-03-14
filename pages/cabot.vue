c6b1f6f2dff2e597049b1e6ed9cb4a43a967269d
fjcabot <fjcabot@hotmail.com>
2024-03-14 15:32:28 +0100

    aa
    pp

---
+++ pages/cabot.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="obtenerCotizacion">pulsa</button>
+   {{  datosjson}}
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'CabotPage',
+  data ()
+  {
+    return{
+      datosjson:[],
+    }
+  },
+  methods:{
+    async obtenerCotizacion(){
+       const response = await this.$axios.$get('http://localhost:8000/shop');
+        this.datosjson = response;
+      
+    },
+  }
+}
+</script>
+
+
---
+++ plugins/axios.js
@@ -0,0 +1,8 @@
+// plugins/axios.js
+import axios from 'axios'
+
+export default ({ $axios }) => {
+  // Configuración global de Axios, si es necesario
+  // $axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
+  
+}
